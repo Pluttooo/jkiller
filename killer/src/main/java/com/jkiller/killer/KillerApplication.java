67d2d4f50da0ae07b61505f7d395b811f093da3e
Pluttooo <pluttooo@PluttooodeMacBook-Pro.local>
2022-09-20 04:26:40 +0800

    new project

---
+++ killer/src/main/java/com/jkiller/killer/KillerApplication.java
@@ -0,0 +1,13 @@
+package com.jkiller.killer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KillerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KillerApplication.class, args);
+    }
+
+}
