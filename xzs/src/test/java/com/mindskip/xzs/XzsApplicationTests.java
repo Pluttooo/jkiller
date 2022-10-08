ef9da4bb5452f7e6bfe8eb708c07dda3fd9de1f7
Pluttooo <pluttooo@PluttooodeMacBook-Pro.local>
2022-10-09 01:34:17 +0800

    add xzs project

---
+++ xzs/src/test/java/com/mindskip/xzs/XzsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindskip.xzs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XzsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
