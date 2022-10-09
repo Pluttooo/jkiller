fa993338e3c56efc7b199c53f4910ceae5014864
Pluttooo <pluttooo@PluttooodeMBP.lan>
2022-10-09 22:25:40 +0800

    新增utility

---
+++ xzs/src/main/java/com/mindskip/xzs/utility/ErrorUtil.java
@@ -0,0 +1,7 @@
+package com.mindskip.xzs.utility;
+
+public class ErrorUtil {
+    public static String parameterFormat(String field, String msg) {
+        return "【" + field + ": " + msg + "】";
+    }
+}
---
+++ xzs/src/main/java/com/mindskip/xzs/utility/PageInfoHelper.java
@@ -0,0 +1,28 @@
+package com.mindskip.xzs.utility;
+import com.github.pagehelper.PageInfo;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class PageInfoHelper {
+    public static <T, J> PageInfo<J> copyMap(PageInfo<T> source, Function<? super T, ? extends J> mapper) {
+        PageInfo<J> newPage = new PageInfo<>();
+        newPage.setPageNum(source.getPageNum());
+        newPage.setPageSize(source.getPageSize());
+        newPage.setSize(source.getSize());
+        newPage.setStartRow(source.getStartRow());
+        newPage.setEndRow(source.getEndRow());
+        newPage.setPages(source.getPages());
+        newPage.setPrePage(source.getPrePage());
+        newPage.setNextPage(source.getNextPage());
+        newPage.setIsFirstPage(source.isIsFirstPage());
+        newPage.setIsLastPage(source.isIsLastPage());
+        newPage.setHasPreviousPage(source.isHasPreviousPage());
+        newPage.setHasNextPage(source.isHasNextPage());
+        newPage.setNavigatePages(source.getNavigatePages());
+        newPage.setNavigatepageNums(source.getNavigatepageNums());
+        newPage.setNavigateFirstPage(source.getNavigateFirstPage());
+        newPage.setNavigateLastPage(source.getNavigateLastPage());
+        newPage.setList(source.getList().stream().map(mapper).collect(Collectors.toList()));
+        return newPage;
+    }
+}
