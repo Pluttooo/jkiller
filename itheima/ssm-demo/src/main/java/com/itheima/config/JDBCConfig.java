6f9f634d632f318e982cd075624bb0a94ebad83d
Pluttooo <pluttooo@PluttooodeMini.lan>
2022-11-09 04:34:05 +0800

    delete unuse import

--- itheima/ssm-demo/src/main/java/com/itheima/config/JDBCConfig.java
+++ itheima/ssm-demo/src/main/java/com/itheima/config/JDBCConfig.java
@@ -1,7 +1,6 @@
 package com.itheima.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import com.sun.tools.corba.se.idl.constExpr.Or;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
