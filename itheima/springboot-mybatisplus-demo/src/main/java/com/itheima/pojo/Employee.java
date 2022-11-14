b93556916aa8b2a56aea6fdbb9bda8d354ca146f
Pluttooo <pluttooo@PluttooodeMacBook-Pro.local>
2022-11-14 17:35:16 +0800

    set TableField

--- itheima/springboot-mybatisplus-demo/src/main/java/com/itheima/pojo/Employee.java
+++ itheima/springboot-mybatisplus-demo/src/main/java/com/itheima/pojo/Employee.java
@@ -1,5 +1,7 @@
 package com.itheima.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -8,13 +10,16 @@ import org.springframework.context.annotation.Primary;
 @Data
 @TableName(value = "db1.emp")
 public class Employee {
-    @TableId
+    @TableId(type = IdType.ASSIGN_ID)
     private Integer empId;
     private String empName;
     private Integer jobId;
     private String mgr;
     private String joinDate;
+    @TableField(select = false)
     private Double salary;
     private Double bonus;
     private Integer deptId;
+    @TableField(exist = false)
+    private Long timestamp;
 }
