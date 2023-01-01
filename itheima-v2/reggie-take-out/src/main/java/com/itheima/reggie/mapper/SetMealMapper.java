e7e84d91c25dbc5fb80522a84dea7d0af9895258
Pluttooo <pluttooo@PluttooodeMacBook-Pro.local>
2023-01-01 21:18:08 +0800

    delete some unuseful message

--- itheima-v2/reggie-take-out/src/main/java/com/itheima/reggie/mapper/SetMealMapper.java
+++ itheima-v2/reggie-take-out/src/main/java/com/itheima/reggie/mapper/SetMealMapper.java
@@ -1,6 +1,5 @@
 package com.itheima.reggie.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itheima.reggie.entity.SetMeal;
 import org.apache.ibatis.annotations.Mapper;
 
